2f110b0effd2bcb4dca593fc47e3981890e9d183
clement6494 <94296944+clement6494@users.noreply.github.com>
2021-11-30 11:15:23 +0100

    Add files via upload

---
+++ projet info/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+void partie(int tab[17][17]));
+
+int partie(int tab[17][17]){
+
+    while (fin_partie!== 0){
+        debut_tour();
+        if action==1;
+            pose();
+        if action==2;
+            bouge();
+        if action==3;
+            annule();
+        if action==4;
+            passe();
+        tour+=1;
+        test_fin_partie();
+}
+
+}
+
+void affichage(tab[17]{17});
+int debut_tour(int d);
+
+int debut_tour(int d){
+t_joueur tour_de;
+    if d%2==0;
+        tour_de= joueur1;
+    if d%2==1;
+        tour_de= joueur2;
+
+    affiche_msg_tour(tour_de.nom);
+    scanf(("%d",&action);
+    if (tour_de.barriere==0){
+        while ((action>1)&&(action<=4)){
+        scanf("%d",&action);}
+    }
+    else{
+    while ((action>=1)&&(action<=4)){
+        scanf("%d",&action);}
+    }
+return (action);
+}
+
+
+
+
